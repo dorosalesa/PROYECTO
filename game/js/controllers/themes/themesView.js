9a6dde9c7e86e4535a55ca08bb56e050f8d324ae
dorosalesa <137126964+dorosalesa@users.noreply.github.com>
2023-06-25 10:27:41 -0600

    update

---
+++ game/js/controllers/themes/themesView.js
@@ -0,0 +1,22 @@
+import { View } from "../view.js";
+
+export class MenuView extends View {
+	constructor(controller,parent) {
+		super(controller, parent);
+		this.controller.className='menuView';
+
+		var title = document.createElement('p');
+		title.innerHTML='Menu';
+		this.container.appendChild(title);
+
+		var loginButton = document.createElement('div');
+		this.container.appendChild(loginButton);
+		loginButton.innerHTML='Login';
+		loginButton.className='game-button';
+
+		var playButton = document.createElement('div');
+		this.container.appendChild(playButton);
+		playButton.innerHTML='Play';
+		playButton.className='game-button';
+	}
+}
