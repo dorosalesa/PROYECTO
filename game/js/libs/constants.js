a339e757338f95a374e0a894419bcbf84a0425e5
dorosalesa <137126964+dorosalesa@users.noreply.github.com>
2023-07-12 15:45:13 -0600

    update

--- game/js/libs/constants.js
+++ game/js/libs/constants.js
@@ -5,3 +5,11 @@ export const DIFFICULTY_STATE = 4;
 export const THEMES_STATE = 5;
 export const SCORES_STATE = 6;
 export const CREDITS_STATE = 7;
+
+export const DIFFICULTY_LOW = 1;
+export const DIFFICULTY_MEDIUM = 2;
+export const DIFFICULTY_HIGH = 3;
+
+export const THEME_FACES = 1;
+export const THEME_FOOD = 2;
+export const THEME_FLAGS = 3;
