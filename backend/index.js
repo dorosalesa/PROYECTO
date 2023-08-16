7592850d82245988bf718b38139c8bede1218058
dorosalesa <137126964+dorosalesa@users.noreply.github.com>
2023-08-16 17:55:59 -0600

    update

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,38 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
-app.get("/cards", (req, res) => {
-  res.send("Hello Cards!");
+app.use(cors());
+
+const food = [
+  "🍇",
+  "🍉",
+  "🍊",
+  "🍋",
+  "🍌",
+  "🍍",
+  "🥭",
+  "🍎",
+  "🍐",
+  "🍑",
+  "🍒",
+  "🍓",
+  "🥝",
+  "🍅",
+  "🥥",
+  "🥑",
+];
+
+app.get("/cards/:difficulty/:theme", (req, res) => {
+  var cards = [];
+  if (req.params !== null) {
+    if (req.params.difficulty !== null && req.params.type !== null) {
+      const difficulty = req.params.difficulty;
+      const type = req.params.type;
+    }
+  }
+  res.send(JSON.stringify.apply(cards));
 });
 
 app.get("/scores", (req, res) => {
--- game/js/controllers/play/playService.js
+++ game/js/controllers/play/playService.js
@@ -8,7 +8,7 @@ export class PlayService extends Service {
 
   getCards(difficulty, theme) {
     var cards = [];
-    var url = `https://us-central1-cenfoprojectsbackend.cloudfunctions.net/app/cards/${difficulty}/type/${theme}`;
+    var url = `http://localhost:3000/cards/${difficulty}/${theme}`;
     var request = new XMLHttpRequest();
     request.open("get", url);
     request.onload = () => {
