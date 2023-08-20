bce6463579f9c66689d92f7ab47790729e5de58a
dorosalesa <137126964+dorosalesa@users.noreply.github.com>
2023-08-20 14:35:34 -0600

    difficulty

--- game/js/libs/constants.js
+++ game/js/libs/constants.js
@@ -6,9 +6,9 @@ export const THEMES_STATE = 5;
 export const SCORES_STATE = 6;
 export const CREDITS_STATE = 7;
 
-export const DIFFICULTY_LOW = 4;
-export const DIFFICULTY_MEDIUM = 6;
-export const DIFFICULTY_HIGH = 8;
+export const DIFFICULTY_LOW = 8;
+export const DIFFICULTY_MEDIUM = 10;
+export const DIFFICULTY_HIGH = 12;
 
 export const THEME_FRUITS = "fruits";
 export const THEME_ANIMALS = "animals";
