eba3af3befa809cd9e61b6715d5f714067a96f08
dorosalesa <137126964+dorosalesa@users.noreply.github.com>
2023-08-21 20:31:58 -0600

    update

--- game/js/controllers/play/playService.js
+++ game/js/controllers/play/playService.js
@@ -8,8 +8,6 @@ export class PlayService extends Service {
 
   getCards(difficulty, theme) {
     var cards = [];
-    //var url = `http://us-central1-cenfoprojectsbackend.cloudfunctions.net/app/cards/${difficulty}/type/${theme}`;
-    //var url = `http://localhost:3000/cards/${difficulty}/${theme}`;
     var url = `https://diana-memorygame.vercel.app/cards/${difficulty}/${theme}`;
     var request = new XMLHttpRequest();
     request.open("get", url);
@@ -29,7 +27,7 @@ export class PlayService extends Service {
   }
 
   sendScore(score, clicks, time, username) {
-    var url = `https://us-central1-cenfoprojectsbackend.cloudfunctions.net/app/scores`;
+    var url = `http://localhost:3000/score`;
 
     var request = new XMLHttpRequest();
     request.open("POST", url);
